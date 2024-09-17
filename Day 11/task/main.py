24f07129beb44ad6aa0c668da9240dbabfca9f52
Anubhav Ranjan <60106478+Mr-AB007@users.noreply.github.com>
2024-09-17 21:37:56 +0530

    Blackjack Project refactor

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -1,6 +1,26 @@
 import random
 from art import logo
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+def deal_card():
+    """Return a random card Drawn from deck of cards"""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    return random.choice(cards)
+
+def final_result(u_score,c_score):
+    """Print the final result of the Game"""
+    if u_score == c_score:
+        return "Draw 🙃"
+    elif c_score == 0:
+        return "Lose, opponent has Blackjack 😱"
+    elif u_score == 0:
+        return "Win with a Blackjack 😎"
+    elif u_score > 21:
+        return "You went over. You lose 😭"
+    elif c_score > 21:
+        return "Opponent went over. You win 😁"
+    elif u_score > c_score:
+        return "You win 😃"
+    else:
+        return "You lose 😤"
 
 wannaPlay = input("Wanna play Blackjack ? y/n : ")
 while wannaPlay == "y":
@@ -8,11 +28,13 @@ while wannaPlay == "y":
     print(logo)
     computer_card = []
     user_card = []
-    computer_card.append(random.choice(cards))
-    user_card.append(random.choice(cards))
-    user_card.append(random.choice(cards))
+
+    computer_card.append(deal_card())
+    user_card.append(deal_card())
+    user_card.append(deal_card())
+
     userSum = user_card[0] + user_card[1]
-    computersum = computer_card[0]
+    computerSum = computer_card[0]
 
     draw = "y"
     while draw == "y":
@@ -20,7 +42,7 @@ while wannaPlay == "y":
         print(f"Computer first card : {computer_card[0]}")
         draw = input("Wanna darw(y) or Pass(n): ")
         if draw == "y":
-            value = random.choice(cards)
+            value = deal_card()
             if value == 11 and (userSum+value) > 21:
                 userSum += 1
                 user_card.append(1)
@@ -30,27 +52,18 @@ while wannaPlay == "y":
         if userSum > 21:
             draw = "n"
     if userSum <= 21:
-        while computersum < 17:
-            value = random.choice(cards)
+        while computerSum < 17:
+            value = int( deal_card())
             if value == 11 and (userSum + value) > 21:
-                computersum += 1
+                computerSum += 1
                 computer_card.append(1)
             else:
-                computersum += value
+                computerSum += value
                 computer_card.append(value)
 
     print(f"Your Final Cards {user_card} , Final Score {userSum}")
-    print(f"Computer's Final Hands{computer_card}, Final score: {computersum}")
+    print(f"Computer's Final Hands{computer_card}, Final score: {computerSum}")
+    print(final_result(user_card,computer_card, userSum, computerSum))
 
-    if userSum == computersum:
-        print("It's Draw ")
-    elif computersum >21:
-        print("Opponent went Over! You Won")
-    elif userSum > 21:
-        print("You went Over! Computer Won")
-    elif userSum > computersum:
-        print("You won")
-    else:
-        print("Computer won")
     wannaPlay = input("Wanna play jackBlack ? y/n : ")
 
